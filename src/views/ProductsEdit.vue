6de6964bdb45bdf0f56fe703c9d069bce1fbcf58
nospame <mapson.evan@gmail.com>
2022-04-13 10:45:55 -0700

    add supplier id to products edit

--- src/views/ProductsEdit.vue
+++ src/views/ProductsEdit.vue
@@ -9,11 +9,13 @@ export default {
       },
       editProductImages: [],
       newProductImages: '',
+      suppliers: [],
       errors: []
     };
   },
   created: function () {
     this.showProduct();
+    this.indexSuppliers();
   },
   methods: {
     showProduct: function () {
@@ -22,6 +24,13 @@ export default {
           console.log(response.data);
           this.editProductParams = response.data;
           this.editProductImages = this.editProductParams.images;
+        })
+    },
+    indexSuppliers: function () {
+      axios.get('/suppliers.json')
+        .then(response => {
+          console.log(response.data);
+          this.suppliers = response.data;
         })
     },
     updateProduct: function (product) {
@@ -57,6 +66,17 @@ export default {
     </div>
     <div> Price:
       <input type="text" v-model="editProductParams.price">
+    </div>
+    <div> Supplier ID:
+      <input type="text" v-model="editProductParams.supplier_id">
+    </div>
+    <div>
+      <label for="suppliers">Supplier:</label>
+      <select name="suppliers" id="suppliers" v-model="editProductParams.supplier_id">
+        <option v-for="supplier in suppliers" v-bind:value="supplier.id">{{
+          supplier.name
+        }}</option>
+      </select>
     </div>
     <button type="submit" v-on:click="updateProduct(editProductParams)">Submit</button>
 
