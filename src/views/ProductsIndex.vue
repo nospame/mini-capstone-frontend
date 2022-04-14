eb9a07edfc862aee96545531c613385e7da74629
nospame <mapson.evan@gmail.com>
2022-04-14 09:43:29 -0700

    add frontend search

--- src/views/ProductsIndex.vue
+++ src/views/ProductsIndex.vue
@@ -4,7 +4,8 @@ export default {
   data: function () {
     return {
       message: "Welcome to the Products Index",
-      products: []
+      products: [],
+      searchTerm: ''
     };
   },
   created: function () {
@@ -17,6 +18,13 @@ export default {
           console.log(response.data);
           this.products = response.data;
         })
+    },
+    filterProducts: function () {
+      return this.products.filter(product => {
+        let lowerName = product.name.toLowerCase();
+        let lowerSearchTerm = this.searchTerm.toLowerCase();
+        return lowerName.includes(lowerSearchTerm);
+      })
     }
   },
 };
@@ -25,8 +33,11 @@ export default {
 <template>
   <div class="products-index">
     <h1>{{ message }}</h1>
-    <router-link to="/products/new">Add a New Product</router-link>
-    <div v-for="product in products">
+    Search: <input type="text" v-model="searchTerm">
+    <p>
+      <router-link to="/products/new">Add a New Product</router-link>
+    </p>
+    <div v-for="product in filterProducts()">
       <img v-bind:src="product.images[0].url" style="max-width: 85%">
       <h3>{{ product.name }}</h3>
       <p>${{ product.price }}</p>
